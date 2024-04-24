9592e29305454c76bb2ead990cc1905fda0a1875
Kylie Bemis <k.bemis@northeastern.edu>
2024-04-24 12:19:16 -0400

    add startup message

--- R/CardinalWorkflows.R
+++ R/CardinalWorkflows.R
@@ -14,3 +14,13 @@ exampleMSIData <- function(name = c("cardinal", "pig206", "rcc"))
 	utils::data(list=name, package="CardinalWorkflows", envir=env)
 	as(env[[name]], "MSImagingExperiment")
 }
+
+# if explicitly attached, describe the vignettes
+
+.onAttach <- function(libname, pkgname) {
+	packageStartupMessage(
+		"Use browseVignettes('CardinalWorkflows') to see the workflows:\n",
+		"1. Segmentation: vignette('MSI-segmentation')\n",
+		"2. Classification: vignette('MSI-classification')\n",
+		"3. Class comparison: vignette('MSI-testing')\n")
+}
